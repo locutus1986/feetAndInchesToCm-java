1973fe84466df2f8bb91d1a6f93f710195fa53cc
locutus1986 <locutus1986@localhost.localdomain>
2019-02-27 13:49:00 -0500

    fixed some things

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,33 +3,31 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        double feetInch = CalFeetandInchesToCm(1, 5);
-        double inch = CalFeetandInchesToCm(0);
+        double feetInch = CalFeetandInchesToCm(-9, 11);
+        double inch = CalFeetandInchesToCm(82);
 
         System.out.println(feetInch);
         System.out.println(inch);
     }
     public static double CalFeetandInchesToCm(int feet, int inches){
-        if(feet <= 0){
-            return -1;
-        }
-        if(inches <= 0 || inches >=12) {
+        if(feet < 0 || inches < 0 || inches >= 12){
             return -1;
         }
 
-        return (feet * 30.48) + (inches * 2.54);
+
+        return (feet * 12 * 2.54) + (inches * 2.54);
     }
 
     public static double CalFeetandInchesToCm(int inches){
         double result;
 
-        if(inches <= 0){
+        if(inches < 0){
             return -1;
         }
 
         int feet = inches / 12;
-        int inch = inches % 12;
+        int remaininginches = inches % 12;
 
-        return CalFeetandInchesToCm(feet, inch);
+        return CalFeetandInchesToCm(feet, remaininginches);
     }
 }
